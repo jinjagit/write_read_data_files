9437a2cfbc0f2f6c60a9e5afdfb29c030c446d97
Simon Tharby <simontharby@gmail.com>
2023-10-09 21:11:48 +0100

    example of large(ish) amount of data

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 `master` - minimal example of serializing to binary, writing to file, opening & reading the file, and deserializing the data.  
 `big` - tests of larger data structure
+
+Not benchmarked, but saving 2 vecs of 1 million floats, in a struct, to file ...
+```Rust
+struct TwoVecs {
+    x: Vec<f32>,
+    y: Vec<f32>,
+}
+```
+... then reading the data back from file and deserializing, takes less than second, and the resulting file is just 8MB.
--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,35 @@ use bincode::serialize_into;
 use std::io::{BufReader, BufWriter};
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
-struct Pair {
-    x: f32,
-    y: f32,
+struct TwoVecs {
+    x: Vec<f32>,
+    y: Vec<f32>,
 }
-
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
-struct MyStruct(Vec<Pair>);
 
 // Minimal example of serializing to binary, writing to file, opening & reading the file, and deserializing the data.
 // See 'big' branch for tests of larger data structure.
 
 fn main() {
-    // define some data in a MyStruct struct
-    let my_struct = MyStruct(vec![Pair { x: 0.0, y: 8.0 }, Pair { x: 10.0, y: 20.5 }]);
+    // define a large amount of data in a MyStruct struct
+    let mut my_struct = TwoVecs{x : vec![], y : vec![]};
+
+    for i in 0..1000000 {
+        my_struct.x.push(i as f32);
+        my_struct.y.push((i + 1) as f32);
+    }
+
+    println!("{:?}", my_struct.x[0]);
 
     // write the data to a file
-    let mut file_to_write_to = BufWriter::new(File::create("foo.dat").unwrap());
+    let mut file_to_write_to = BufWriter::new(File::create("big.dat").unwrap());
     serialize_into(&mut file_to_write_to, &my_struct).unwrap();
 
     // drop the open file from scope
     drop(file_to_write_to);
 
     // read the data back from the file
-    let mut file_to_read_from = BufReader::new(File::open("foo.dat").unwrap());
-    let deserialized_struct : MyStruct = bincode::deserialize_from(&mut file_to_read_from).unwrap();
+    let mut file_to_read_from = BufReader::new(File::open("big.dat").unwrap());
+    let deserialized_struct : TwoVecs = bincode::deserialize_from(&mut file_to_read_from).unwrap();
 
-    println!("{:?}", deserialized_struct)
+    println!("{:?}", deserialized_struct.x[999999]);
 }
